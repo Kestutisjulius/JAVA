30d0905966819478a784283134679a8ac141e2fe
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-09-01 12:38:17 +0300

    variables

---
+++ src/main/java/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotalNumber = myFirstNumber+mySecondNumber+myThirdNumber;
+
+        System.out.println("my first Total number = " + (myTotalNumber));
+
+    }
+}
